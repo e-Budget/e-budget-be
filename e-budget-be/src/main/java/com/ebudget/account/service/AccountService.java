8458d67cf8b78a9f86d1412abe988fd3ad0cc3f4
Leandro Pereira <leojgpereira@gmail.com>
2025-05-24 13:14:32 +0100

    [FIX] Fix field injection issues

--- e-budget-be/src/main/java/com/ebudget/account/service/AccountService.java
+++ e-budget-be/src/main/java/com/ebudget/account/service/AccountService.java
@@ -5,15 +5,15 @@ import com.ebudget.account.repository.AccountRepository;
 import com.ebudget.account.resource.request.NewAccountDTO;
 import com.ebudget.account.resource.response.AccountDTO;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 
 @ApplicationScoped
+@RequiredArgsConstructor
 public class AccountService {
     private static final Double INITIAL_BALANCE = 0.0;
 
-    @Inject
-    AccountRepository accountRepository;
+    private final AccountRepository accountRepository;
 
     @Transactional
     public AccountDTO addAccount(NewAccountDTO newAccountDTO) {
